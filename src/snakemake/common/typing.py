43aee39749dd3e6e3c8e884602c3757ad2d8ea13
Jared Lumpe <jared@jaredlumpe.com>
2025-10-25 23:41:15 -0700

    StrPath path alias

--- src/snakemake/common/typing.py
+++ src/snakemake/common/typing.py
@@ -1,5 +1,9 @@
-from typing import FrozenSet, Set, TypeVar, Union
+from typing import FrozenSet, Set, TypeAlias, TypeVar, Union
+from os import PathLike
 
 
 T = TypeVar("T")
 AnySet = Union[Set[T], FrozenSet[T]]
+
+#: File system path as string or pathlike object.
+StrPath: TypeAlias = str | PathLike
